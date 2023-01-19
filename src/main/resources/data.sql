6512608aef47518a6ffb312a3e79e78b7df2f3e9
SerhiiSentiurin <sergeysenturinn1998@gmail.com>
2023-01-19 02:57:41 +0200

    moved to liquibase

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Dnipro', 'Kyiv', '2023-01-30','10:00:00', 120.00, 30);
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Kyiv', 'Dnipro', '2023-01-30','12:00:00', 120.00, 30);
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Dnipro', 'Kyiv', '2023-01-30','15:00:00', 120.00, 30);
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Kyiv','Dnipro', '2023-01-30','19:00:00', 120.00, 30);
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Dnipro', 'Kyiv', '2023-02-01','10:00:00', 120.00, 20);
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Dnipro', 'Kyiv', '2023-02-01','15:00:00', 120.00, 20);
-INSERT INTO routes (departure_city, arrival_city, departure_date, departure_time, cost, available_seats) VALUES ('Kyiv', 'Dnipro', '2023-02-02','12:00:00', 120.00, 30);
